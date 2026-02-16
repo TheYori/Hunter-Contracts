c0034f560265ec02fd18cbe6fe010a4815611129
TheYori <rickiguldborg40@gmail.com>
2026-02-16 02:57:49 +0100

    Test of login (Post) hunter
    16/02/26 - 02:57
    -------------------
    Tested POST for hunter login with Postman: Successful

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,19 @@
 import { Router, Request, Response } from 'express'
 import { createCreature, getAllCreatures, getCreatureById, updateCreatureById, deleteCreatureById } from './controllers/creatureController';
-import { registerHunter } from './controllers/authController';
+import { loginHunter, registerHunter } from './controllers/authController';
 
 const router: Router = Router();
 
 //Basic CRUD: get, post, put, delete
 
 router.get('/', (req: Request, res: Response) => {
-    //Succes status - 200 means "OK"
+    //Success status - 200 means "OK"
     res.status(200).send('welcome to the Hunter API') 
 });
 
 //Authenticate
 router.post('/hunter/register', registerHunter);
+router.post('/hunter/login', loginHunter)
 
 //Create
 router.post('/creatures', createCreature);
